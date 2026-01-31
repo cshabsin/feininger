ded41da4672d54b6c98c05d0e147d1b57defbb83
Chris Shabsin <cshabsin@gmail.com>
2026-01-30 22:50:54 -0500

    Refine ground and sea textures with perspective and softer details
    - Soften sea strokes with Gaussian blur and remove slant/jagged triangles
    - Implement perspective-based ground facets with a cohesive dark/brown palette
    - Add thin, reed-like grass tufts that scale with distance
    Gemini commit: refine visual tweaks

--- app/page.tsx
+++ app/page.tsx
@@ -119,7 +119,8 @@ export default function Home() {
                     <fePointLight x="-5000" y="-10000" z="20000" />
                   </feSpecularLighting>
                   <feComposite in="light" in2="distorted" operator="arithmetic" k1="0" k2="1" k3="1" k4="0" result="painted" />
-                  <feComposite in="painted" in2="distorted" operator="in" />
+                  <feComposite in="painted" in2="distorted" operator="in" result="comp" />
+                  <feGaussianBlur in="comp" stdDeviation="0.8" />
                 </filter>
 
                 {/* Filter 2: Ground Strokes (More textured/rougher) */}
