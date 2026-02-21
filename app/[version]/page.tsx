35a18c75eafc55731656214e9e5865b299fa504d
Chris Shabsin <cshabsin@gmail.com>
2026-02-21 17:51:45 -0500

    Refactor to use dynamic routes for versions.
    Gemini commit: let's make the different versions into separate urls so that it's possible to navigate directly to the one yo uwant to see

---
+++ app/[version]/page.tsx
@@ -0,0 +1,15 @@
+import VersionClient from './VersionClient';
+
+type Version = 'v1' | 'v2' | 'gemini3';
+
+export async function generateStaticParams() {
+  return [
+    { version: 'v1' },
+    { version: 'v2' },
+    { version: 'gemini3' },
+  ]
+}
+
+export default function VersionPage({ params }: { params: { version: Version } }) {
+  return <VersionClient version={params.version} />;
+}
